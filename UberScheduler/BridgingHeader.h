ab04a2859d272d30905a8b1efb4d6d08585c27f4
Canzhi Ye <canzhiye@airbears2-10-142-34-96.airbears2.1918.berkeley.edu>
2015-04-11 20:44:46 -0700

    actually gonna use Carthage instead of submodules

---
+++ UberScheduler/BridgingHeader.h
@@ -0,0 +1,10 @@
+//
+//  BridgingHeader.h
+//  UberScheduler
+//
+//  Created by Canzhi Ye on 4/11/15.
+//  Copyright (c) 2015 Canzhi Ye. All rights reserved.
+//
+
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import "Lockbox.h"
---
+++ UberScheduler/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  UberScheduler
+//
+//  Created by Canzhi Ye on 4/11/15.
+//  Copyright (c) 2015 Canzhi Ye. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewModel: NSObject {
+    let loginPressed: RACCommand!
+}
